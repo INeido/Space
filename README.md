6dec98e5666fefddd41c54c39ddcc7656a47cc98
INeido <58654074+INeido@users.noreply.github.com>
2021-04-20 15:45:11 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Space
+
+
 Doesn't do much. Just some bodies flying around in the void.
+
+## Settings
+
+| Name | Description | Standard Value |
+| ------ | ------ | ------ |
+| FPS | Pretty self explanatory, isn't it? | 120 |
+| drag | "Air"-Drag, but in space | 0.000001 |
+| coll_loss | Loss of speed on collision | 0.000001 |
+| res_x | Changes the screen width | 1920 |
+| res_y | Do I have to tell you? | 1080 |
+| zoom_factor | Zoooooooom | 0.98 |
+
+## Switches
+
+| Name | Description |
+| ------ | ------ |
+| trace | Enables cool effect |
+| edges | If enabled, bodies can't escape screen |
+| gravity | The thing with the apple |
+| collision_color | Bodies change colors on collision |
+
+## Controls
+
+| Button | Description |
+| ------ | ------ |
+| ESC | Closes game |
+| E | Resets bodies |
+| S | Toggles between **Insert** and **Focus** mode |
+| Left Click | Follows body when clicked (Focus) |
+| A & D | Switch between bodies (Focus) |
+| Left Click | Gravitational pull at cursor (Insert) |
+| Right Click | Gravitational push at cursor (Insert) |
+| Middle Click | Drag screen (Insert) |
+
+## Examples
+
+Kinda stable orbit with 3 bodies
+```
+bodies.append(Body(p=(0, 300), v=(3, 0), r=5, c=white))
+bodies.append(Body(p=(0 + 150, 200), v=(3, 0), r=5, c=white))
+bodies.append(Body(p=(0, 0), r=75))
+```
+
+Not really stable orbit with 2 bodies
+```
+bodies.append(Body(p=(100, - 100), v=(3, 2), r=25, c=white))
+bodies.append(Body(p=(0, 0), r=50))
+```
+
+2 bodies on collision course
+```
+bodies.append(Body(p=(-600, 0), r=50, v=(3, 0)))
+bodies.append(Body(p=(600, 0), r=50, v=(-3, 0)))
+```
+
+Create any amount of random bodies defined in **count**. (Don't recommend more than 50)
+```
+count = 10
+
+rand = random.choices(population=[["5"], ["10"], ["15"], ["20"], ["30"]],
+                      weights=[0.35, 0.30, 0.20, 0.1, 0.05],
+                      k=count)
+
+for x in range(count):
+    bodies.append(Body(p=(random.randint(-res_x / 2, res_x / 2),
+                          random.randint(-res_y / 2, res_y / 2)),
+                       v=(random.randint(-5, 5),
+                          random.randint(-5, 5)),
+                       r=int(int(''.join(rand[x]))),
+                       c=white))
+```
+
+You can past these into the **reset** function.
---
+++ main.py
@@ -0,0 +1,417 @@
+import sys
+import itertools
+import random
+
+import numpy
+import pygame
+
+# Constants
+G = 6.673 * (10 ** -11)
+
+offset_x = 0
+offset_y = 0
+zoom = 1
+
+# Settings
+FPS = 120
+
+drag = 0.000001
+coll_loss = 0.000001
+
+res_x = 1920
+res_y = 1080
+
+zoom_factor = 0.98
+
+# Colors
+white = pygame.Color(255, 255, 255)
+black = pygame.Color(0, 0, 0)
+gray = pygame.Color(128, 128, 128)
+
+# Initiate Pygame
+screen = pygame.display.set_mode((res_x, res_y), pygame.DOUBLEBUF|pygame.OPENGL|pygame.NOFRAME)
+screen_rect = screen.get_rect()
+screen.set_alpha(None)
+
+display = pygame.Surface((res_x, res_y))
+display_rect = display.get_rect()
+display.set_colorkey(black)
+
+pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP, pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP])
+
+bodies = []
+traces = []
+#trails = [] TO-DO
+
+# Switches
+trace = False
+edges = False
+gravity = True
+collision_color = False
+
+usr_id = 999999999
+
+# Initiate Game
+pygame.init()
+screen = pygame.display.set_mode((res_x, res_y))
+pygame.display.set_caption('Space')
+fpsClock = pygame.time.Clock()
+
+
+class Body:
+    id_iter = itertools.count()
+
+    def __init__(self, c=gray, r=25, p=(0, 0), v=(0, 0)):
+        """
+        c -- color (default gray)
+        r -- radius (default 25)
+        p -- cartesian position x, y (default 0, 0)
+        ap -- afterimage position x, y (default 0, 0)
+        v -- velocity x,y (default 0.0)
+        s -- size
+        m -- mass
+        """
+
+        self.id = next(self.id_iter)
+
+        self.c = c
+        self.r = r
+        self.fr = r * zoom
+        self.p = p
+        self.ap = p
+        self.v = v
+        self.m = 2 * (10 ** 9) * numpy.pi * (self.r ** 2)
+
+    def draw(self):
+
+        pygame.draw.circle(display, self.c, cptosp(self.p), self.fr)
+
+    def tracer(self):
+        # Velocity cant be zero and radius has to be bigger than 1.5
+        if self.v[0] != 0 and self.v[1] != 0 and self.fr >= 1.5 and trace:
+            traces.append(Trace(self.p, numpy.subtract(self.c, (35, 35, 35, 0)), self.fr, self.v))
+
+    def zoom(self, direction):
+        self.r = self.r / direction
+        self.fr = self.r * zoom
+        self.v = numpy.divide(self.v, direction)
+        self.m = 2 * (10 ** 9) * numpy.pi * (self.r ** 2)
+
+        return []
+
+    def gravity(self):
+        for body in bodies:
+
+            if self == body:
+                continue
+
+            disx, disy = numpy.subtract(body.p, self.p)
+
+            d = numpy.sqrt((disx ** 2) + (disy ** 2))
+
+            sumr = self.r + body.r
+
+            angle = numpy.arctan2(disy, disx)
+
+            # Collision detection
+            if d <= sumr and self.id != usr_id and body.id != usr_id:
+                self.collision(body, angle)
+                break
+
+            if gravity:
+                f = G * body.m * self.m / (d ** 2)
+                self.v = (numpy.cos(angle) * f) / self.m + self.v[0], (numpy.sin(angle) * f) / self.m + self.v[1]
+
+        limit = 60
+        if self.v[0] >= limit:
+            self.v = limit, self.v[1]
+        if self.v[0] <= -limit:
+            self.v = -limit, self.v[1]
+        if self.v[1] >= limit:
+            self.v = self.v[0], limit
+        if self.v[1] <= -limit:
+            self.v = self.v[0], -limit
+
+        self.p = numpy.add(self.p, numpy.subtract(self.v, numpy.multiply(self.v, drag)))
+
+    def edge(self):
+        p = cptosp(self.p)
+
+        if p[0] <= 0:
+            self.p = sptocp((res_x, res_y - p[1]))
+        elif p[0] >= res_x:
+            self.p = sptocp((0, res_y - p[1]))
+
+        if p[1] <= 0:
+            self.p = sptocp((res_x - p[0], res_y))
+        elif p[1] >= res_y:
+            self.p = sptocp((res_x - p[0], 0))
+
+    def collision(self, body, angle):
+        vel0 = numpy.sqrt((self.v[1] ** 2) + (self.v[0] ** 2))
+        vel1 = numpy.sqrt((body.v[1] ** 2) + (body.v[0] ** 2))
+
+        movangle0 = numpy.arctan2(self.v[1], self.v[0])
+        movangle1 = numpy.arctan2(body.v[1], body.v[0])
+
+        newvelx0 = ((vel0 * numpy.cos(movangle0 - angle) * (self.m - body.m) + (
+                2 * body.m * vel1 * numpy.cos(movangle1 - angle))) / (self.m + body.m)) * numpy.cos(angle) + (
+                               vel0 * numpy.sin(movangle0 - angle) * numpy.cos(angle + (numpy.pi / 2)))
+        newvely0 = ((vel0 * numpy.cos(movangle0 - angle) * (self.m - body.m) + (
+                2 * body.m * vel1 * numpy.cos(movangle1 - angle))) / (self.m + body.m)) * numpy.sin(angle) + (
+                               vel0 * numpy.sin(movangle0 - angle) * numpy.sin(angle + (numpy.pi / 2)))
+
+        newvelx1 = ((vel1 * numpy.cos(movangle1 - angle) * (body.m - self.m) + (
+                    2 * self.m * vel0 * numpy.cos(movangle0 - angle))) / (body.m + self.m)) * numpy.cos(angle) + (
+                               vel1 * numpy.sin(movangle1 - angle) * numpy.cos(angle + (numpy.pi / 2)))
+        newvely1 = ((vel1 * numpy.cos(movangle1 - angle) * (body.m - self.m) + (
+                    2 * self.m * vel0 * numpy.cos(movangle0 - angle))) / (body.m + self.m)) * numpy.sin(angle) + (
+                               vel1 * numpy.sin(movangle1 - angle) * numpy.sin(angle + (numpy.pi / 2)))
+
+        self.v = (newvelx0 - (newvelx0 * coll_loss), newvely0 - (newvely0 * coll_loss))
+        body.v = (newvelx1 - (newvelx1 * coll_loss), newvely1 - (newvely1 * coll_loss))
+
+        if collision_color:
+            self.c = pygame.Color(random.randint(40, 255), random.randint(40, 255), random.randint(40, 255))
+            self.c = pygame.Color(random.randint(40, 255), random.randint(40, 255), random.randint(40, 255))
+
+    def overlap(self):
+        for body in bodies:
+
+            if body == self:
+                continue
+
+            disx, disy = numpy.subtract(body.p, self.p)
+
+            d = numpy.sqrt((disx ** 2) + (disy ** 2))
+            sumr = self.r + body.r
+
+            if d >= sumr:
+                continue
+
+            angle = numpy.arctan2(disy, disx)
+
+            # Disposition if overlapping (px - (radia - distance * cos(angle), py - (radia - distance * sin(angle)
+            self.p = numpy.subtract(self.p, ((sumr - d) * numpy.cos(angle), (sumr - d) * numpy.sin(angle)))
+
+
+class Trace:
+    def __init__(self, p, c, fr, v):
+        """
+        p -- cartesian position x, y (default 0, 0)
+        c -- color (default gray)
+        fr -- fake radius (default 25)
+        v -- velocity x,y (default 0.0)
+        """
+
+        self.p = p
+        self.c = c
+        self.fr = fr
+        self.v = v
+
+        if 1 < int(-self.v[0]):
+            vx = random.randint(1, int(-self.v[0])) / 12
+        else:
+            vx = random.randint(int(-self.v[0]), 1) / 12
+        if 1 < int(-self.v[1]):
+            vy = random.randint(1, int(-self.v[1])) / 12
+        else:
+            vy = random.randint(int(-self.v[1]), 1) / 12
+
+        self.v = vx, vy
+        self.fr = random.randint(1, int(self.fr / 1.5))
+
+    def update(self):
+        self.p = numpy.add(self.v, self.p)
+        self.fr -= 0.5
+
+    def draw(self):
+        pygame.draw.circle(display, self.c, cptosp(self.p), self.fr)
+
+def sptocp(p):
+    cartesianx = (p[0] - offset_x - res_x / 2) / zoom
+    cartesiany = (p[1] - offset_y - res_y / 2) / -zoom
+
+    return cartesianx, cartesiany
+
+def cptosp(p):
+    screenx = res_x / 2 + zoom * p[0] + offset_x
+    screeny = res_y / 2 - zoom * p[1] + offset_y
+
+    return screenx, screeny
+
+def reset():
+    bodies = []
+    traces = []
+
+    bodies.append(Body(p=(100, - 100), v=(3, 2), r=25, c=white))
+    bodies.append(Body(p=(0, 0), r=50))
+
+    return bodies, traces
+
+
+bodies, traces = reset()
+
+# Flags and temps
+firstpos = 0
+s_pressed = False
+focused_body = None
+
+# Game Loop
+while True:
+    fpsClock.tick(FPS)
+    display.fill((15, 15, 15))
+    m_spos = pygame.mouse.get_pos()
+    m_cpos = sptocp(m_spos)
+
+    # Move usr body to cursor position
+    if bodies[-1].id == usr_id:
+        try:
+            bodies[-1].p = m_cpos
+        except AttributeError:
+            pass
+
+    # Screen drag
+    if firstpos != 0:
+        offset_x, offset_y = numpy.add(numpy.subtract(m_spos, firstpos), (oldoffset_x, oldoffset_y))
+
+    # Adjust focus
+    if focused_body != None:
+        offset_x = -focused_body.p[0]
+        offset_x *= zoom
+        offset_y = focused_body.p[1]
+        offset_y *= zoom
+
+    # Quit Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        # Toggle between Insert and Focus mode
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                s_pressed = not s_pressed
+                focused_body = None
+
+        # Reset
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_e:
+                bodies, traces = reset()
+
+        # Focus mode
+        if s_pressed:
+            if event.type == pygame.KEYUP:
+                # Switch between bodies
+                if event.key == pygame.K_d:
+
+                    if focused_body == None:
+                        focused_body = bodies[0]
+                    if focused_body.id != len(bodies)-1:
+                        focused_body = bodies[focused_body.id+1]
+                    else:
+                        focused_body = bodies[0]
+
+                elif event.key == pygame.K_a:
+                    if focused_body == None:
+                        focused_body = bodies[-1]
+                    if focused_body.id != 0:
+                        focused_body = bodies[focused_body.id-1]
+                    else:
+                        focused_body = bodies[-1]
+
+            # Finding body per click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    for body in bodies:
+                        dx, dy = numpy.abs(numpy.subtract(m_cpos, body.p))
+                        R = body.r
+                        if dx + dy <= R:
+                            focused_body = body
+                        elif dx ** 2 + dy ** 2 <= R ** 2:
+                            focused_body = body
+                # Little bit of debugging on right click
+                elif event.button == 3:
+                    print(fpsClock.get_fps())
+                    print(len(traces))
+
+        # Interaction mode
+        else:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    bodies.append(Body(p=m_cpos, c=black, r=2))
+                    bodies[-1].m = (2 * (10 ** 9) * numpy.pi * (90 ** 2))
+                    bodies[-1].id = usr_id
+
+                elif event.button == 3:
+                    bodies.append(Body(p=m_cpos, c=black, r=2))
+                    bodies[-1].m = -(2 * (10 ** 9) * numpy.pi * (90 ** 2))
+                    bodies[-1].id = usr_id
+
+            if event.type == pygame.MOUSEBUTTONUP and bodies[-1].id == usr_id:
+                if event.button != 4 and event.button != 5:
+                    bodies.remove(bodies[-1])
+
+        # Drag screen
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 2:
+                oldoffset_x = offset_x
+                oldoffset_y = offset_y
+                firstpos = m_spos
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 2:
+                firstpos = 0
+
+        # Zoom
+        if event.type == pygame.MOUSEWHEEL and not edges:
+            if event.y == 1:
+                for body in bodies:
+                    zoom /= zoom_factor
+                    offset_x /= zoom_factor
+                    offset_y /= zoom_factor
+                    traces = body.zoom(zoom_factor)
+            if event.y == -1:
+                for body in bodies:
+                    zoom /= (1 / zoom_factor)
+                    offset_x /= (1 / zoom_factor)
+                    offset_y /= (1 / zoom_factor)
+                    traces = body.zoom((1 / zoom_factor))
+
+    # Delete small traces
+    for trace in traces:
+        trace.update()
+        if trace.fr <= 0.99:
+            traces.remove(trace)
+
+    # Update bodies
+    for body in bodies:
+        # Get Position and Velocity
+        body.gravity()
+
+        # Edge Detection
+        if edges:
+            body.edge()
+
+        # Overlap Protection
+        body.overlap()
+
+        # Tracing
+        if body.id != usr_id:
+            body.tracer()
+
+    # Draws traces
+    for trace in traces:
+        trace.draw()
+
+    # Draws bodies
+    for body in bodies:
+        body.draw()
+
+    screen.blit(display, (0, 0))
+    pygame.display.update()
+    pygame.display.flip()
